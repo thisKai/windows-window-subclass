90e181621b6f6a470970e83796e8da1136d7f6ce
kai <thiskaiguy@gmail.com>
2020-02-19 14:42:35 +0000

    Create a winit window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+    platform::windows::WindowBuilderExtWindows,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_transparent(true)
+        .with_no_redirection_bitmap(true)
+        .build(&event_loop)
+        .unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
 }
