00b78be61fb4a849cb736335487d1868e431c72e
Kai <thiskaiguy@gmail.com>
2020-03-08 05:50:26 -0700

    Make window metrics return Err on non-windows

--- src/extend_frame.rs
+++ src/extend_frame.rs
@@ -112,34 +112,41 @@ impl Margins {
     }
 }
 
-pub fn window_frame_metrics() -> WindowFrameMetrics {
-    let (border, titlebar) = unsafe {
-        let mut border = RECT {
-            left: 0,
-            top: 0,
-            right: 0,
-            bottom: 0,
-        };
-        AdjustWindowRectEx(&mut border, WS_OVERLAPPEDWINDOW & !WS_CAPTION, FALSE, NULL as _);
+pub fn window_frame_metrics() -> Result<WindowFrameMetrics, &'static str> {
+    #[cfg(not(windows))]
+    {
+        Err("not windows")
+    }
+    #[cfg(windows)]
+    {
+        let (border, titlebar) = unsafe {
+            let mut border = RECT {
+                left: 0,
+                top: 0,
+                right: 0,
+                bottom: 0,
+            };
+            AdjustWindowRectEx(&mut border, WS_OVERLAPPEDWINDOW & !WS_CAPTION, FALSE, NULL as _);
 
-        let mut with_titlebar = RECT {
-            left: 0,
-            top: 0,
-            right: 0,
-            bottom: 0,
+            let mut with_titlebar = RECT {
+                left: 0,
+                top: 0,
+                right: 0,
+                bottom: 0,
+            };
+            AdjustWindowRectEx(&mut with_titlebar, WS_OVERLAPPEDWINDOW, FALSE, NULL as _);
+            (border, with_titlebar.top)
         };
-        AdjustWindowRectEx(&mut with_titlebar, WS_OVERLAPPEDWINDOW, FALSE, NULL as _);
-        (border, with_titlebar.top)
-    };
 
-    WindowFrameMetrics {
-        titlebar: -titlebar,
-        border: Margins {
-            left: -border.left,
-            top: -border.top,
-            right: border.right,
-            bottom: border.bottom,
-        },
+        Ok(WindowFrameMetrics {
+            titlebar: -titlebar,
+            border: Margins {
+                left: -border.left,
+                top: -border.top,
+                right: border.right,
+                bottom: border.bottom,
+            },
+        })
     }
 }
 
