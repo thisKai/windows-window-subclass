0d1454969b67b8f07d612be474e3ff86fa7c27ce
Kai <thiskaiguy@gmail.com>
2020-03-08 08:17:05 -0700

    Split the dwm extend frame example Create a simplified dwm extend frame example Create a separate example for changing state at runtime

--- examples/change_at_runtime.rs
+++ examples/change_at_runtime.rs
@@ -1,5 +1,6 @@
 use {
-    windows_window_subclass::{SetSubclass, DwmFrame},
+    windows_window_subclass::{SetSubclass, DwmFrame, Margins},
+    std::rc::Rc,
     winit::{
         event::{Event, WindowEvent},
         event_loop::{ControlFlow, EventLoop},
@@ -9,13 +10,16 @@ use {
 
 fn main() {
     let event_loop = EventLoop::new();
+
+    let subclass = Rc::new(DwmFrame::sheet());
+
     let window = WindowBuilder::new()
         .with_visible(false)
         .with_transparent(true)
         // .with_no_redirection_bitmap(true)
         .build(&event_loop)
         .unwrap()
-        .with_subclass(DwmFrame::sheet());
+        .with_subclass(subclass.clone());
     window.set_visible(true);
 
     event_loop.run(move |event, _, control_flow| {
@@ -29,6 +33,16 @@ fn main() {
                 WindowEvent::CloseRequested => {
                     *control_flow = ControlFlow::Exit
                 },
+                WindowEvent::Focused(focused) => {
+                    if focused {
+                        subclass.set_margins(Margins::sheet());
+                    } else {
+                        subclass.set_margins(Margins {
+                            top: 100,
+                            ..Default::default()
+                        });
+                    }
+                }
                 _ => (),
             }
             _ => (),
