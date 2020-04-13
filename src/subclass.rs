d963357ff96c3052fad376d6d117935afc018e64
Kai <thiskaiguy@gmail.com>
2020-04-12 23:25:34 -0700

    Remove the extra box

--- src/subclass.rs
+++ src/subclass.rs
@@ -12,18 +12,18 @@ use {
 
 pub fn subclass_win32_window<S: WindowSubclass>(h_wnd: HWND, subclass: S) {
     static mut SUBCLASS_ID: UINT_PTR = 0;
-    let data: Box<Box<dyn WindowSubclass>> = Box::new(Box::new(subclass));
+    let data = Box::new(subclass);
     let data = Box::into_raw(data);
     unsafe {
-        SetWindowSubclass(h_wnd, Some(subclass_wnd_proc), SUBCLASS_ID, data as usize);
+        SetWindowSubclass(h_wnd, Some(subclass_wnd_proc::<S>), SUBCLASS_ID, data as usize);
 
-        let data = &mut **data;
+        let data = &mut *data;
         data.init(h_wnd, SUBCLASS_ID);
         SUBCLASS_ID += 1;
     }
 }
 
-unsafe extern "system" fn subclass_wnd_proc(
+unsafe extern "system" fn subclass_wnd_proc<S: WindowSubclass>(
     h_wnd: HWND,
     message: UINT,
     w_param: WPARAM,
@@ -31,7 +31,7 @@ unsafe extern "system" fn subclass_wnd_proc(
     _u_id_subclass: UINT_PTR,
     dw_ref_data: DWORD_PTR,
 ) -> LRESULT {
-    let data = dw_ref_data as *mut Box<dyn WindowSubclass>;
-    let data = &mut **data;
+    let data = dw_ref_data as *mut S;
+    let data = &mut *data;
     data.wnd_proc(h_wnd, message, w_param, l_param)
 }
